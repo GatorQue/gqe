d2f6b2b8edd24c91f5c064fdcc5876b0bfa12b27
Jacob <Jacob@Jacobs-desktop.home>
2012-04-22 22:14:39 -0700

    Rewrote the Entities system. Now uses an Entity/Component based system.

--- include/GQE/Entities.hpp
+++ include/GQE/Entities.hpp
@@ -1,45 +1,8 @@
-/**
- * Provides the GQE namespace includes for the GQE Entities
- *
- * @file include/GQE/Entities.hpp
- * @author Jacob Dix
- * @date 20110812 - Initial Release
- * @date 20110906 - Change to system include style
- */
-#ifndef   GQE_ENTITIES_HPP_INCLUDED
-#define   GQE_ENTITIES_HPP_INCLUDED
+#ifndef ENTITIES_HPP_INCLUDED
+#define ENTITIES_HPP_INCLUDED
 
-/////////////////////////////////////////////////////////////////////////////
-// Headers
-/////////////////////////////////////////////////////////////////////////////
-#include <GQE/Config.hpp>
-
-// GQE Entities includes
-#include <GQE/Entities/interfaces/IObject.hpp>
-#include <GQE/Entities/classes/ObjectManager.hpp>
-#endif // GQE_ENTITIES_HPP_INCLUDED
-
-/**
- * @defgroup Entities GQE Entities module
- * Provides the GQE Engine with basic game object management as well as collision detection.
- *
- * Copyright (c) 2010-2011 Jacob Dix
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
+#include "interfaces/TProperty.hpp"
+#include "interfaces/IComponent.hpp"
+#include "classes/Entity.hpp"
+#include "Components/TestComponent.hpp"
+#endif
