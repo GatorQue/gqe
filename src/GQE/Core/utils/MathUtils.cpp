ccf6f9b8b782e33e5ad48fd3412951870c08fc67
cloudncali <cloudncali@localhost>
2013-08-09 09:08:21 -0700

    Added MathUtils functions.

---
+++ src/GQE/Core/utils/MathUtils.cpp
@@ -0,0 +1,24 @@
+#include <GQE/Core/utils/MathUtils.hpp>
+
+namespace GQE
+{
+	void SetSeed(GQE::Uint32 theSeed)
+	{
+		srand(theSeed);
+	}
+
+	float Random(float theMin, float theMax)
+	{
+		return static_cast<double>(std::rand()) / RAND_MAX * theMax + theMin;
+	}
+	int Random(int theMin, int theMax)
+	{
+		return static_cast<double>(std::rand()) / RAND_MAX * theMax + theMin;
+	}
+
+	GQE::Uint32 Random(GQE::Uint32 theMin, GQE::Uint32 theMax)
+	{
+		return static_cast<double>(std::rand()) / RAND_MAX * theMax + theMin;
+	}
+
+}
